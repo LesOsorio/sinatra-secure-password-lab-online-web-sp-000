bb49750e1d46604553e416056a141367cf52b3cb
Leslie Osorio <lesosorio17@gmail.com>
2019-12-02 21:03:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < Sinatra::Base
       @user = User.find_by(:username => params[:username])
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
-        user.save
+        #user.save
         redirect '/account'
       else
         redirect '/failure'
